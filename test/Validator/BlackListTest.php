e6c9fe9a7e5d83962c048d8c52532f4d04496439
volkoinen <lucasdearaujo@icloud.com>
2016-03-01 11:02:32 -0300

    Criado teste unitário específico para validação pela regra BlackList.

---
+++ test/Validator/BlackListTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ValidatorTest;
+
+use Collections\ArrayList;
+use Validator\Field;
+use Validator\Rules\BlackList;
+use Validator\Validator;
+use Validator\Validation;
+
+/**
+ * Classe para a validação dos testes na regra de blacklist.
+ *
+ * @author  Lucas A. de Araújo <lucas@painapp.com.br>
+ * @package ValidatorTest
+ */
+class BlackListTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBlackList()
+    {
+        $blackList = new ArrayList();
+        $blackList->addAll(['Lucas', 'Matheus']);
+
+        $validator = new Validator();
+
+        // Verifica se o nome 'Lucas' está permitido.
+        $validator->getValidations()->add(function(Validation $v) use ($blackList) {
+            $v->setField(new Field('nome', 'Lucas'));
+            $v->getRules()->add(new BlackList($blackList));
+        });
+
+        // Verifica se o nome 'Adolf' está permitido.
+        $validator->getValidations()->add(function(Validation $v) use ($blackList) {
+            $v->setField(new Field('nome', 'Adolf'));
+            $v->getRules()->add(new BlackList($blackList));
+        });
+
+        $validator->execute();
+        $nomeNaoPermitido = $validator->getErrors()->first()->getField()->getValue();
+
+        $this->assertEquals($nomeNaoPermitido, 'Lucas');
+        $this->assertEquals($validator->getErrors()->count(), 1);
+    }
+}
